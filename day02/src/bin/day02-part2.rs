3e89bdc3da0e4e26482f646efe749c732193ed98
James Harmison <jharmison@gmail.com>
2025-12-02 10:44:52 -0500

    solve: day02 part2

--- day02/src/bin/day02-part2.rs
+++ day02/src/bin/day02-part2.rs
@@ -1,1 +1,96 @@
+use clap::Parser;
+use clio::Input;
+use rayon::prelude::*;
+use std::io::{self, BufReader, prelude::*};
 
+#[derive(Parser)]
+struct Opt {
+    /// Input file, use '-' for stdin
+    #[clap(value_parser, default_value = "-")]
+    input: Input,
+
+    /// the number of CPU cores to use (all if unspecified)
+    #[clap(short, long, default_value_t = num_cpus::get())]
+    threads: usize,
+}
+
+fn valid_id(id: &usize) -> bool {
+    let string_id = id.to_string();
+    let len = string_id.len();
+    for i in 1..len {
+        let slice = &string_id[0..i];
+        let slice_len = slice.len();
+        let limit = len / slice_len + 1;
+        for j in 2..limit {
+            if string_id == slice.repeat(j) {
+                eprintln!("Found invalid id: {}", id);
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn solve(lines: Vec<String>) -> usize {
+    let mut ret = 0;
+    for range in lines {
+        let (start, end) = range.split_once('-').unwrap();
+        let u_start = start.parse::<usize>().unwrap();
+        let u_end = end.parse::<usize>().unwrap();
+        ret += (u_start..(u_end + 1))
+            .into_par_iter()
+            .filter(|id| !valid_id(id))
+            .sum::<usize>();
+    }
+    ret
+}
+
+fn main() -> io::Result<()> {
+    let opt = Opt::parse();
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(opt.threads)
+        .build_global()
+        .unwrap();
+
+    let reader = BufReader::new(opt.input);
+    let lines: Vec<String> = reader
+        .lines()
+        .flatten()
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+            s.split(',')
+                .map(|st| st.to_string())
+                .collect::<Vec<String>>()
+        })
+        .flatten()
+        .collect();
+    println!("{}", solve(lines));
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn convert_example(example: &[&str]) -> Vec<String> {
+        example.iter().map(|line| line.to_string()).collect()
+    }
+    #[test]
+    fn given() {
+        let example = [
+            "11-22",
+            "95-115",
+            "998-1012",
+            "1188511880-1188511890",
+            "222220-222224",
+            "1698522-1698528",
+            "446443-446449",
+            "38593856-38593862",
+            "565653-565659",
+            "824824821-824824827",
+            "2121212118-2121212124",
+        ];
+        println!("{}", example.join(","));
+        assert_eq!(solve(convert_example(&example)), 4174379265);
+    }
+}
