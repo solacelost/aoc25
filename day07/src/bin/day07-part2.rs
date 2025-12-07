13fe9402c1cebcbc15c15d2cee9c50e222137d8c
James Harmison <jharmison@gmail.com>
2025-12-07 12:06:36 -0500

    feat: rewrite without unused struct complexity

--- day07/src/bin/day07-part2.rs
+++ day07/src/bin/day07-part2.rs
@@ -10,84 +10,52 @@ struct Opt {
     input: Input,
 }
 
-#[derive(Debug, Copy, Clone)]
-enum Position {
-    Empty,
-    Start,
-    Splitter,
-}
-
-#[derive(Debug, Clone)]
-struct Row {
-    columns: Vec<Position>,
-}
-impl Row {
-    fn new() -> Self {
-        Self {
-            columns: Vec::new(),
+fn process(
+    grid: &Vec<Vec<char>>,
+    beam: (usize, usize),
+    cache: &mut Cache<(usize, usize), usize>,
+) -> usize {
+    if beam.1 == grid.len() - 1 {
+        return 1;
+    }
+    if let Some(cached) = cache.get(&beam) {
+        return cached;
+    }
+    let row = &grid[beam.1];
+    let ret = match row[beam.0] {
+        '.' => process(grid, (beam.0, beam.1 + 1), cache),
+        '^' => {
+            process(grid, (beam.0 - 1, beam.1 + 1), cache)
+                + process(grid, (beam.0 + 1, beam.1 + 1), cache)
         }
-    }
-    fn add(&mut self, position: Position) {
-        self.columns.push(position);
-    }
-}
-
-#[derive(Debug)]
-struct Manifold {
-    rows: Vec<Row>,
-}
-impl Manifold {
-    fn new() -> Self {
-        Self { rows: Vec::new() }
-    }
-    fn add(&mut self, row: Row) {
-        self.rows.push(row);
-    }
-    fn process(&self, beam: (usize, usize), cache: &mut Cache<(usize, usize), usize>) -> usize {
-        if beam.1 == self.rows.len() - 1 {
-            return 1;
-        }
-        if let Some(result) = cache.get(&beam) {
-            return result;
-        }
-        let row = &self.rows[beam.1];
-        let ret = match row.columns[beam.0] {
-            Position::Empty => self.process((beam.0, beam.1 + 1), cache),
-            Position::Splitter => {
-                self.process((beam.0 - 1, beam.1 + 1), cache)
-                    + self.process((beam.0 + 1, beam.1 + 1), cache)
-            }
-            _ => panic!(
-                "Somehow hit position '{:?}' at ({}, {})",
-                row.columns[beam.0], beam.0, beam.1
-            ),
-        };
-        cache.insert((beam.0, beam.1), ret);
-        ret
-    }
+        _ => panic!(
+            "Somehow hit position '{:?}' at ({}, {})",
+            row[beam.0], beam.0, beam.1
+        ),
+    };
+    cache.insert((beam.0, beam.1), ret);
+    ret
 }
 
 fn solve(lines: Vec<String>) -> usize {
-    let cache = Cache::new(1_000_000);
+    let cache = Cache::new(10_000);
 
-    let mut manifold = Manifold::new();
+    let mut manifold = Vec::new();
     let mut start: Option<(usize, usize)> = None;
     for (y, line) in lines.iter().enumerate() {
-        let mut row = Row::new();
+        let mut row = Vec::new();
         for (x, chr) in line.chars().enumerate() {
             match chr {
-                '.' => row.add(Position::Empty),
                 'S' => {
-                    row.add(Position::Start);
+                    row.push(chr);
                     start = Some((x, y + 1));
                 }
-                '^' => row.add(Position::Splitter),
-                _ => panic!("This should never happen"),
+                _ => row.push(chr),
             }
         }
-        manifold.add(row);
+        manifold.push(row);
     }
-    manifold.process(start.unwrap(), &mut cache.clone())
+    process(&manifold, start.unwrap(), &mut cache.clone())
 }
 
 fn main() -> io::Result<()> {
