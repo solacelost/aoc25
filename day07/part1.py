02c0a1ebaeb4ea8e4a276b1b0eb0519dfafac5fb
James Harmison <jharmison@gmail.com>
2025-12-07 11:54:14 -0500

    add: comments

--- day07/part1.py
+++ day07/part1.py
@@ -10,24 +10,23 @@ with open(Path(__file__).parent.joinpath("input")) as f:
 
 
 def process(beam: Point) -> int:
-    if beam.y == len(data):
+    if beam.y == len(data):  # we are at the end, no splits left
         return 0
-    if (result := cache.get(beam)) != None:
+    if (result := cache.get(beam)) != None:  # skip double-counting splits
         return 0
     match data[beam.y][beam.x]:
         case ".":
             next = Point(beam.x, beam.y + 1)
-            result = process(next)
+            result = process(next)  # run the beam further down
         case "^":
             left, right = Point(beam.x - 1, beam.y), Point(beam.x + 1, beam.y)
-            result = 1 + process(left) + process(right)
+            result = 1 + process(left) + process(right)  # split the beam, count the split
         case _:
             raise RuntimeError("Can't get here")
-    cache[beam] = result
+    cache[beam] = result  # memoize our non-terminating results
     return result
 
 
 cache: dict[Point, int] = dict()
-start = Point(data[0].find("S"), 1)
-result = process(start)
-print(result)
+start = Point(data[0].find("S"), 1)  # start just below the S at the top
+print(process(start))  # recurse into every path the beam could take, print the number of splits
--- day07/part2.py
+++ day07/part2.py
@@ -10,24 +10,23 @@ with open(Path(__file__).parent.joinpath("input")) as f:
 
 
 def process(beam: Point) -> int:
-    if beam.y == len(data):
+    if beam.y == len(data):  # this path has terminated
         return 1
-    if (result := cache.get(beam)) != None:
+    if (result := cache.get(beam)) != None:  # use memoized results
         return result
     match data[beam.y][beam.x]:
         case ".":
             next = Point(beam.x, beam.y + 1)
-            result = process(next)
+            result = process(next)  # run the beam further down
         case "^":
             left, right = Point(beam.x - 1, beam.y), Point(beam.x + 1, beam.y)
-            result = process(left) + process(right)
+            result = process(left) + process(right)  # split the beam, counting paths
         case _:
             raise RuntimeError("Can't get here")
-    cache[beam] = result
+    cache[beam] = result  # memoize our non-terminating results
     return result
 
 
 cache: dict[Point, int] = dict()
-start = Point(data[0].find("S"), 1)
-result = process(start)
-print(result)
+start = Point(data[0].find("S"), 1)  # start just below the S at the top
+print(process(start))  # recurse into every path the beam could take, print the number of paths
