f1a6ea36c420f96b8bd1b9edaf1b4d78446f2c8d
James Harmison <jharmison@gmail.com>
2025-12-11 10:04:15 -0500

    fix: radically simplify as i get used to the petgraph api

--- day11/src/bin/day11-part1.rs
+++ day11/src/bin/day11-part1.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use clio::Input;
 use petgraph::algo::simple_paths::all_simple_paths;
 use petgraph::graph::{DiGraph, NodeIndex};
+use std::collections::HashMap;
 use std::collections::hash_map::RandomState;
 use std::io::{self, BufReader, prelude::*};
 
@@ -17,33 +18,25 @@ struct Opt {
 }
 
 fn solve(lines: Vec<String>) -> usize {
-    let mut wiring = DiGraph::<usize, ()>::new();
-    let mut map: Vec<(&str, Vec<&str>)> = lines
+    let mut wiring = DiGraph::<&str, ()>::new();
+    let mut map: HashMap<&str, Vec<&str>> = lines
         .iter()
         .map(|line| {
             let (node, relationships) = line.split_once(":").unwrap();
             (node, relationships.split_whitespace().collect())
         })
         .collect();
-    map.push(("out", Vec::new()));
-    let nodes: Vec<NodeIndex> = (0..map.len()).map(|i| wiring.add_node(i)).collect();
-    map.iter()
-        .enumerate()
-        .for_each(|(src, (_, relationships))| {
-            for other in relationships.iter() {
-                let dest = map
-                    .iter()
-                    .position(|&(othernode, _)| &othernode == other)
-                    .unwrap();
-                wiring.add_edge(nodes[src], nodes[dest], ());
-            }
-        });
-    let you = nodes[map
+    map.insert("out", Vec::new());
+    let nodes: HashMap<&str, NodeIndex> = map
         .iter()
-        .position(|&(othernode, _)| othernode == "you")
-        .unwrap()];
-    let out = nodes[map.len() - 1];
-    all_simple_paths::<Vec<_>, _, RandomState>(&wiring, you, out, 0, None).count()
+        .map(|(&node, _)| (node, wiring.add_node(node)))
+        .collect();
+    map.iter().for_each(|(src, relationships)| {
+        for dest in relationships.iter() {
+            wiring.add_edge(nodes[src], nodes[dest], ());
+        }
+    });
+    all_simple_paths::<Vec<_>, _, RandomState>(&wiring, nodes["you"], nodes["out"], 0, None).count()
 }
 
 fn main() -> io::Result<()> {
